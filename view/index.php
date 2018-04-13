4343dced4b1038463dd526c11be1ce5b739ef78b
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-04-13 15:26:58 +1000

    Create index.php

---
+++ view/index.php
@@ -0,0 +1,185 @@
+    <header>
+
+        <div class="wellcom text-center align-middle ">
+            <h1 class="">مرحبا بكم في عالمنا</h1>
+
+            <h2>نحن جدا سعداء اذا فكرت بالانضمام الينا</h2>
+        </div>
+
+    </header>
+    <div class="title-arch">البرمجة من اجل العراق
+        <p>CODE FOR IRAQ</p>
+    </div>
+    <div class="row">
+        <div class="col-lg-10 mx-auto text-right">
+            <p class="large text-muted" dir="rtl"> وهي مبادرة إنسانية غير ربحية تهدف الى خدمة المجتمع عن طريق البرمجة Programming. تعتبر "Code For IRAQ" مبادرة
+                تعليمية حقيقية ترعى المهتمين بتعلم تصميم وبرمجة تطبيقات الهاتف الجوال ومواقع الانترنت وبرامج الحاسوب والشبكات
+                والاتصالات ونظم تشغيل الحاسوب باستخدام التقنيات مفتوحة المصدر Open Source ، كما توفر لهم جميع الدروس التعليمية
+                اللازمة وبشكل مجاني تماماً ، حيث يتسنى لطالب العلم أن يكتسب الخبرة العملية أثناء وبعد فترة الدراسة وذلك ليسهل
+                عليه ايجاد الوظيفة وبشكل سريع بعد التخرّج وتهدف هذه المبادرة الى تحويل المجتمع من مجتمع سلبي مستهلك الى مجتمع
+                ايجابي منتج يعتمد بالدرجة الاساس على استثمار العقول والخبرات والمهارات بغض النظر عن العمر او التخصص بل الاهم
+                من ذلك تعتمد على مبدأ المواطنة والمشاركة الفاعلة في تأسيس وبناء المجتمع تدعو هذه المبادرة جميع الطلبة والخريجين
+                والهواة والأساتذة الجامعيين والمهتمين مجال البرمجة وتقنيات المعلومات وكذلك الاختصاصات الاخرى الى التطوع والمشاركة
+                الفعلية لأجل الارتقاء بواقع البلد ، حيث تعتبر فرصة عظيمة لاكتساب الخبرة والمهارة عن طريق تصميم وتنفيذ برامج
+                وتطبيقات خدمية من شأنها خدمة المواطن وذلك ضمن مجاميع عمل نشطة وفعّالة يتعاون فيها جميع الأفراد كفريق واحد
+                يتبادل الاراء والخبرات ويطرح الافكار لتتم مناقشتها وتطبيقها على أرض الواقع ، كما تفتح المجال لجميع المواطنين
+                العراقيين ومن جميع الاختصاصات إلى المشاركة الفاعلة في هذه المشاريع لرفد الفريق بالخبرات والافكار والاراء
+                والمقترحات التي من شأنها خدمة المجتمع بأفضل ما يمكن. لا يخفى على أحد أن لمثل هذه المبادرات دور رئيسي في تحسين
+                ورفع اقتصاد البلد ، حيث يتم فيها إبراز المقدرة الحقيقية والذكاء الحقيقي للمواطن العراقي مما يساعد على استقطاب
+                الخبرات المحلية وتوظيفها بدلاً عن استيراد الخبرات الاجنبية والتي عادة ما تكون مكلفة جداً للشركات والمؤسسات
+                الحكومية والأهلية وجميع القطاعات العاملة في البلد والتي قد لا تظاهي أحيانا خبرة وكفاءة وإخلاص المواطن العراقي
+                ، كما إن سيطرة الشركات والعمالات الأجنبية على سوق العمل العراقي ساهم في تقليص فرص التوظيف بشكل هائل نتج عنه
+                عدد كبير من الافراد العاطلين عن العمل من ذوي الخبرات والشهادات والاختصاصات المختلفة ، وهنا يأتي دور الجامعات
+                والمؤسسات والشركات العراقية في التثقيف والتطوير والمشاركة في تغيير وتحسين القوة الاقتصادية والعلمية للبلد
+                ورفع مكانته بين البلدان الاخرى.
+            </p>
+        </div>
+    </div>
+    <div id="services"></div>
+    <section class="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h1 class="section-heading text-uppercase">الخدمات</h1>
+                    <h2 class="section-subheading text-muted">نسعى من اجل خدمة ابناء محافظة واسط مجانا وبدون مقابل</h3>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fas fa-code-branch fa-stack-1x"></i>
+                    </span>
+                    <h4 class="service-heading">الحلول البرمجية</h4>
+                    <p class="text-muted" dir="rtl">نقدم كل مايخص المواطن الواسطي وجميع الامور التي تسهل الحياة ونسعى لتحشيد كافة الطاقات من اجل خدمة شعب
+                        واسط برمجيا
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fab fa-github fa-stack-1x"></i>
+                    </span>
+                    <h4 class="service-heading">مشاريع مفتوحة المصدر</h4>
+                    <p class="text-muted" dir="rtl">جميع المشاريع التي نقدمها هي مشاريع مفتوحة المصدر يمكن لجميع من يحتاجها اعادةاستخدامها لخدمة ابناء وطنة
+                        مجانا
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fas fa-lock fa-stack-1x "></i>
+                    </span>
+                    <h4 class="service-heading">التدريب المستمر</h4>
+                    <p class="text-muted" dir="rtl">نضمن التدريب المستمر لجميع اعضاء الفريق وفق اخر التقنيات واطر العمل ونسعى لتحقيق فريق متكامل يتمكن جميع
+                        اعضائه الوقوف على ارجلهم والانطلاق نحو سوق العمل الحر</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- our path -->
+
+    <div id="about"></div>
+    <section>
+        <div class="container services">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h1 class="section-heading text-uppercase">مسارنا</h1>
+                    <h2 class="section-subheading text-muted">فريق واسط هو فريق ناشئ وجزء لايتجزأ من مبادر البرمجة من اجل العراق</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="timeline">
+                        <li>
+                            <div class="timeline-image">
+                                <img class="rounded-circle img-fluid" src="<?php echo $this->_static("img/about/1.jpg"); ?>" alt="">
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>2017</h4>
+                                    <h4 class="subheading">بدايتنا المتواضعة</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted" dir="rtl">انطلعقت مبادرة البرمجة من اجل العراق في عام 2017 وكان لواسط الحظ الاوفر في استجابة راعي
+                                        المبادرة ( المهندس محمد عيسى) لاعضاء الفريق في رغبتهم في خدمة ابناء محافظتهم وتكوين
+                                        الفريق والتوجيه لاختيار المسار التدريبي
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="timeline-inverted">
+                            <div class="timeline-image">
+                                <img class="rounded-circle img-fluid" src="<?php echo $this->_static("img/about/2.jpg"); ?>" alt="">
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>2018</h4>
+                                    <h4 class="subheading">انطلاق فترة التدريب المعمق </h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted" dir="rtl">نشهد الان انطلاق فترة التدريب المعمق التي بموجبها سيتم تدريب الفريق على انجاز مشاريع
+                                        حقيقية وفرض مهام على كل عضو لاداء اعمال برمجية لصقل المهارات الخاصة بهم وتوجيههم
+                                        نحو الطريق الصحيح
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="timeline-image">
+                                <img class="rounded-circle img-fluid" src="<?php echo $this->_static("img/about/3.jpg"); ?>" alt="">
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>2019</h4>
+                                    <h4 class="subheading">مرحلة تنفيذ الافكار والمشاريع</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">سيتم في هذه الفترة المضي قدما من اجل المباشرة بتنفيذ المشاريع والافكار المطروحة والتي
+                                        تخدم ابناء محافظة واسط برمجيا لتسهيل الحياه اليومية سواء في مجال الويب او تطبيقات
+                                        الموبايل
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+
+
+                        <li class="timeline-inverted">
+                            <div class="timeline-image">
+                                <br>
+                                <h3>كن
+                                    <br>جزءاً
+                                    <br>من فريقنا</h3>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <div id="regester"></div>
+    <section>
+        <div class="regester services text-center">
+            <p>
+                <h1>
+                    انضم الينا
+                </h1>
+                <h2>
+                    اذا كنت من سكنة محافظة واسط وتمتلك الرغبة في ان تكون فردا منتجا وتحب الخير لمجتمعك
+                </h2>
+                <h2>
+                    يمكنك ارسال رسالة الى
+                    <br>
+                    <span>nael.fadhil@codeforiraq.org</span>
+                    <br> تتضمن سيرتك الذاتية مهما كانت وسيتم التواصل معك في اقرب وقت ممكن
+                </h2>
+            </p>
+        </div>
+    </section>
+
+   
+    

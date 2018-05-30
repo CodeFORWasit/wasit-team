57573a73033d30aedaca1954a552efa0851e811f
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-05-31 03:40:35 +1000

    Update navbar.php

--- view/layout/navbar.php
+++ view/layout/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar  navbar-expand-sm navbar-dark bg-dark fixed-top">
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark <?php echo isset($this->_fixed) ? $this->_fixed : ""; ?>" dir="rtl">
 
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
         aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -6,24 +6,25 @@
         <i class="fa fa-bars"></i>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav text-uppercase mr-auto">
+    <div class="collapse navbar-collapse" id="navbarResponsive" dir="rtl">
+        <ul class="navbar-nav">
 
-        <?php 
-        if (isset($this->_navbar)){
-            foreach ($this->_navbar as $key => $value) {
-        
-        ?>
-            <li class="nav-item">
-                <a class="nav-link js-scroll-trigger" href="<?php echo $key ?>">
-                    <?php echo $value ?>
-                </a>
-            </li>
-        <?php
-            }
-        }
-        ?>
+            <?php 
+                if (isset($this->_navbar)){
+                    foreach ($this->_navbar as $key => $value) {
+
+            ?>
+                <li class="">
+                    <a class="nav-link js-scroll-trigger" href="<?php echo $key ?>">
+                        <?php echo $value ?>
+                    </a>
+                </li>
+            <?php
+                    }
+                }
+            ?>
         </ul>
     </div>
-    <a class="navbar-brand ml-auto navbar-right js-scroll-trigger " href="/shoping">البرمجة من اجل العراق | فريق واسط</a>
+     <a class="navbar-brand ml-auto navbar-right js-scroll-trigger " href="/c4w">البرمجة من اجل العراق | فريق واسط</a>
+   
 </nav>

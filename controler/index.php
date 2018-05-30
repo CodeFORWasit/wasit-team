9e9788f4877388e302c5c40a168b22365db6e427
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-05-31 03:28:29 +1000

    Update index.php

--- controler/index.php
+++ controler/index.php
@@ -6,10 +6,18 @@ class index extends Controler{
         parent::__construct();
     }
     
+    // Main PAGE FOR ' class index '  
+    // route " / " 
     function index(){
-        
+
         # Title Page
         $this->view->_title = "CODE FOR WASIT";
+        
+                
+        
+        $this->view->_fixed = "fixed-top";
+        
+        # NAVBAR ul->li  
         $this->view->_navbar  = array(
                 "#services" => "الخدمات",
                 "#about"    => "مسارنا",
@@ -17,12 +25,12 @@ class index extends Controler{
                 "#regester" => "التسجيل"
         );
 
-        # Body 
+        #  VIEW FILE  
         $this->view->render(
                     "index",
                     $_header="layout/header",
                     $_foter="layout/footer"
-            );
+        );
     }
 
 }

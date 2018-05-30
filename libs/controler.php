c7ba3fa866dcedb9f44bf196f6b5a6b7205fd0ed
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-05-31 03:25:43 +1000

    Update and rename Controler.php to controler.php

--- libs/controler.php
+++ libs/controler.php
@@ -1,14 +1,17 @@
 <?php
 
 class Controler {
+
+
     
     public function __construct() {
         
         $this->view = new View();
+        $this->method = $_SERVER["REQUEST_METHOD"];
         
     }
     
-    function loadModel($name,$path_model="models/"){
+    function loadModel($name,$path_model=MODEL){
         $nameModel = $name."_model";
         $path_models = $path_model.$nameModel.".php";
         if(file_exists($path_models)){

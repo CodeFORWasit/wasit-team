3055ed89f542adbf33d996d58973b6bd720c84d1
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-05-31 03:41:57 +1000

    Update footer.php

--- view/layout/footer.php
+++ view/layout/footer.php
@@ -1,11 +1,13 @@
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
      	
-        <script type="text/javascript" src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
-        <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+ 
 
-        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+ 
 
         <script type="text/javascript" src="<?php echo $this->_static('js/ajax.web.js'); ?>"></script>
         <script type="text/javascript" src="<?php echo $this->_static('js/mjs.js'); ?>"></script>

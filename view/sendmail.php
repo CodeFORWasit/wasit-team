0065ccacc83c2ac68b3f6ad427013b92bab6cb2d
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-05-31 03:43:42 +1000

    Create sendmail.php

---
+++ view/sendmail.php
@@ -0,0 +1,18 @@
+
+<br>
+<div class="container jumbotron" style="
+        margin-top: 50px;background-color: #333;color: white;
+        border-radius: 10px;text-align: center;box-shadow: 0 20px 20px 0  #333;
+        " >
+    <h3>
+        
+        Send Message To Your Mail 
+        <strong class="btn-link " style="color:white;"> 
+            <?php echo $this->mail;  ?> 
+        </strong> 
+        
+        
+    </h3>
+    <a href="signup/resend" > اعادة الارسال  </a>
+
+</div>

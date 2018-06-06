623d51fb51563395e55044f4f962b501511017a5
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-06-07 06:08:09 +1000

    Update index.php

--- controler/index.php
+++ controler/index.php
@@ -22,6 +22,7 @@ class index extends Controler{
                 "#services" => "الخدمات",
                 "#about"    => "مسارنا",
                 "teams"     => "الفريق",
+                "links"     => "links",
                 "#regester" => "التسجيل"
         );
 
